f7110327975ea558184768c9239d11bdc1d9e6b6
alex.zhou <33326664@qq.com>
2020-02-14 21:11:14 +0800

    feat unit: add convert float to unit string

--- pkg/unit.go
+++ pkg/unit.go
@@ -2,29 +2,46 @@ package pkg
 
 import (
 	"errors"
+	"fmt"
 	"math/big"
 )
 
-func ConvertUintToBigInt(num int, unit string) (*big.Int, error) {
-	kInt, _ := new(big.Int).SetString("1000", 10)
-	mInt, _ := new(big.Int).SetString("1000000", 10)
-	gInt, _ := new(big.Int).SetString("1000000000", 10)
-	tInt, _ := new(big.Int).SetString("1000000000000", 0)
-	pInt, _ := new(big.Int).SetString("1000000000000000", 0)
-	eInt, _ := new(big.Int).SetString("1000000000000000000", 0)
-	zInt, _ := new(big.Int).SetString("1000000000000000000000", 0)
-	yInt, _ := new(big.Int).SetString("1000000000000000000000000", 0)
+var (
+	kInt, _ = new(big.Int).SetString("1000", 10)
+	mInt, _ = new(big.Int).SetString("1000000", 10)
+	gInt, _ = new(big.Int).SetString("1000000000", 10)
+	tInt, _ = new(big.Int).SetString("1000000000000", 0)
+	pInt, _ = new(big.Int).SetString("1000000000000000", 0)
+	eInt, _ = new(big.Int).SetString("1000000000000000000", 0)
+	zInt, _ = new(big.Int).SetString("1000000000000000000000", 0)
+	yInt, _ = new(big.Int).SetString("1000000000000000000000000", 0)
 
-	var unitMaps map[string]*big.Int = map[string]*big.Int{
+	unitMaps map[string]*big.Int = map[string]*big.Int{
 		"K": kInt, "M": mInt,
 		"G": gInt, "T": tInt,
 		"P": pInt, "E": eInt,
 		"Z": zInt, "Y": yInt,
 	}
 
+)
+
+
+func ConvertUintToBigInt(num int, unit string) (*big.Int, error) {
 	if val, ok := unitMaps[unit]; !ok {
 		return nil, errors.New("invalid unit")
 	} else {
-		return new (big.Int).Mul(big.NewInt(int64(num)), val), nil
+		return new(big.Int).Mul(big.NewInt(int64(num)), val), nil
 	}
+}
+
+func ConvertFloatToUnitString(num float64, format string, unit string) (string, error) {
+	var result string
+	if val, ok := unitMaps[unit]; !ok {
+		return "", errors.New("invalid unit")
+	} else {
+		numFloat64 := new(big.Float).SetFloat64(num)
+		quo, _ := new(big.Float).Quo(numFloat64, new(big.Float).SetInt(val)).Float64()
+		result = fmt.Sprintf(format, quo)
+	}
+	return fmt.Sprintf("%s%s", result, unit), nil
 }
--- pkg/unit_test.go
+++ pkg/unit_test.go
@@ -11,7 +11,20 @@ func TestConvertUintToBigInt(t *testing.T) {
 	}
 
 	if unitBigInt.String() != "100000000000000" {
-		t.Errorf("invalid transaction id")
+		t.Errorf("invalid result")
+		t.Fail()
+	}
+}
+
+func TestConvertFloatToUnitString(t *testing.T) {
+	floatNum := 1.546609893555465e+13
+	unitString, err := ConvertFloatToUnitString(floatNum, "%.8f", "E")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if unitString != "15.47T" {
+		t.Errorf("invalid result")
 		t.Fail()
 	}
 }
